2862fbec26242b0081ca782addd4ab9f5b763cdd
Lucas Latsch <bello-lucas@hotmail.com>
2025-01-17 19:01:04 -0300

    Add search results display and styling to Search page

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,16 +1,29 @@
-import React from "react";
-
+import styles from "./Search.module.css";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
 import { useQuery } from "../../hooks/useQuery";
+import PostDetail from "../../components/PostDetail/PostDetail";
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocument("posts", search);
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.nopost}>
+            <p>Não foram encontrados posts com essa tag...</p>{" "}
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
